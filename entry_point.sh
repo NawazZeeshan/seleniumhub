57f05cb9792ef64c76a336fc275e0dd234a96757
mouyigang <mouyigang@gmail.com>
2017-04-04 15:14:42 +0100

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -17,7 +17,7 @@ function shutdown {
     echo "shutdown complete"
 }
 
-java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+$JAVA_HOME/bin/java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
   -role hub \
   -hubConfig $CONF \
   ${SE_OPTS} &
